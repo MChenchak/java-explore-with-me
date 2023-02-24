24fddfcb61b8e68f46edad53a9049f038f56e004
vtb4055920 <vtb4055920@region.vtb.ru>
2023-02-24 22:42:08 +0300

    Изменен статус http ответа при успешном создании пользователя на 201

--- ewm/src/main/java/ru/practicum/user/controller/UserController.java
+++ ewm/src/main/java/ru/practicum/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.user.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.user.dto.UserDto;
 import ru.practicum.user.service.UserService;
@@ -27,6 +28,7 @@ public class UserController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDto createUser(@Valid @RequestBody UserDto userDto) {
         log.info("create user");
         return userService.createUser(userDto);
