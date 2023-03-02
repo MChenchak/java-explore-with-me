fdb47f6e1ead2bf8237cfbf61022039133ed77bd
vtb4055920 <vtb4055920@region.vtb.ru>
2023-03-02 20:35:37 +0300

    реализована фича -комментарии

--- ewm/src/main/java/ru/practicum/comment/controller/UserCommentController.java
+++ ewm/src/main/java/ru/practicum/comment/controller/UserCommentController.java
@@ -1,6 +1,7 @@
 package ru.practicum.comment.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.comment.dto.CommentDto;
 import ru.practicum.comment.service.CommentService;
@@ -20,6 +21,7 @@ public class UserCommentController {
     }
 
     @PostMapping("/{eventId}")
+    @ResponseStatus(HttpStatus.CREATED)
     public CommentDto createComment(@Valid @RequestBody CommentDto commentDto,
                                     @Positive @PathVariable Long userId,
                                     @Positive @PathVariable Long eventId) {
